899f72597f05eef03e858fb3e5d6059f804e7675
Diegodfp <diegodfp.fly@gmail.com>
2024-08-16 17:21:33 -0500

    first commit

---
+++ app/(routes)/about-me/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { Avatar } from "@/components/avatar";
+import ContainerPage from "@/components/container-page";
+import CounterServices from "@/components/counter-services";
+import { CoverParticles } from "@/components/cover-particles";
+import TimeLine from "@/components/time-line";
+import TransitionPage from "@/components/transition-page";
+
+const AboutMePage = () => {
+    return (
+        <>
+        <div className="flex min-h-screen bg-gradient-cover bg-no-repeat">
+            <TransitionPage />
+            <CoverParticles />
+            <ContainerPage>
+                <Avatar />
+                <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">
+                    Toda mi{' '}
+                    <span className="font-bold text-secondary">
+                        trayectoria profesional
+                    </span>
+                </h1>
+
+                <CounterServices />
+
+                <TimeLine />
+
+            </ContainerPage>
+            
+            </div>
+        </>
+    );
+}
+
+export default AboutMePage;
